fb6e2b8eb7116bbbc96b669be7d2c277bac5d1d3
WolfgangAli <wolfgang520@outlook.com>
2025-06-21 23:14:48 -0600

    Primer commit

---
+++ src/app/details/details.ts
@@ -0,0 +1,72 @@
+import {Component, inject} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {ActivatedRoute} from '@angular/router';
+import {HousingService} from '../housing';
+import {HousingLocationInfo} from '../housinglocation';
+import {FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
+
+@Component({
+  selector: 'app-details',
+  imports: [CommonModule, ReactiveFormsModule],
+  template: `
+    <article>
+      <img
+        class="listing-photo"
+        [src]="housingLocation?.photo"
+        alt="Exterior photo of {{ housingLocation?.name }}"
+        crossorigin
+      />
+      <section class="listing-description">
+        <h2 class="listing-heading">{{ housingLocation?.name }}</h2>
+        <p class="listing-location">{{ housingLocation?.city }}, {{ housingLocation?.state }}</p>
+      </section>
+      <section class="listing-features">
+        <h2 class="section-heading">Acerca de esta casa</h2>
+        <ul>
+          <li>Unidades disponibles: {{ housingLocation?.availableUnits }}</li>
+          <li>Tiene Wi-Fi? {{ housingLocation?.wifi }}</li>
+          <li>Tiene lavanderia? {{ housingLocation?.laundry }}</li>
+        </ul>
+      </section>
+      <section class="listing-apply">
+        <h2 class="section-heading">Aplica para vivir aqui</h2>
+        <form [formGroup]="applyForm" (submit)="submitApplication()">
+          <label for="first-name">Nombre</label>
+          <input id="first-name" type="text" formControlName="firstName" />
+          <label for="last-name">Apellido</label>
+          <input id="last-name" type="text" formControlName="lastName" />
+          <label for="email">Email</label>
+          <input id="email" type="email" formControlName="email" />
+          <button type="submit" class="primary">Aplicar</button>
+        </form>
+      </section>
+    </article>
+  `,
+  styleUrls: ['details.css'],
+})
+
+export class Details {
+  route: ActivatedRoute = inject(ActivatedRoute);
+  housingService = inject(HousingService);
+  housingLocation: HousingLocationInfo | undefined;
+  applyForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl(''),
+  });
+  
+  constructor() {
+    const housingLocationId = parseInt(this.route.snapshot.params['id'], 10);
+    this.housingService.getHousingLocationById(housingLocationId).then((housingLocation) => {
+      this.housingLocation = housingLocation;
+    });
+  }
+
+  submitApplication() {
+    this.housingService.submitApplication(
+      this.applyForm.value.firstName ?? '',
+      this.applyForm.value.lastName ?? '',
+      this.applyForm.value.email ?? '',
+    );
+  }
+}
